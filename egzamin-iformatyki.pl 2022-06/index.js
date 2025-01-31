b05b1103b80ea0e923d587dbac22bc0e0c90211f
Zhenia1303 <koorama005@gmail.com>
2025-01-31 15:30:09 +0100

    Add files via upload

--- egzamin-iformatyki.pl 2022-06/index.js
+++ egzamin-iformatyki.pl 2022-06/index.js
@@ -1,6 +1,9 @@
-function clear() {
-    debugger
+function delet() {
     document.getElementById("a").value = "";
     document.getElementById("b").value = "";
     document.getElementById("c").value = "";
+}
+
+function href() {
+    window.location.href = "kwerenda.txt"
 }
