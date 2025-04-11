2f1fc562e1691cc098679420b81158c5364928a2
Zhenia1303 <koorama005@gmail.com>
2025-04-11 08:37:57 +0200

    Delete egzamin-iformatyki.pl 2022-06 directory

--- egzamin-iformatyki.pl 2022-06/index.js
+++
@@ -1,9 +0,0 @@
-function delet() {
-    document.getElementById("a").value = "";
-    document.getElementById("b").value = "";
-    document.getElementById("c").value = "";
-}
-
-function href() {
-    window.location.href = "kwerenda.txt"
-}
